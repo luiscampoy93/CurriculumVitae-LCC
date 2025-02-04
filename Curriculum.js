05d8c83e30296bee4645d5c7c890194f96d7be3d
luiscampoy93 <luis_campoy18@hotmail.com>
2025-02-04 20:07:46 +0100

    Add files via upload

---
+++ Curriculum.js
@@ -0,0 +1,12 @@
+const botonEmail = document.getElementById('botonmail')
+
+botonEmail.addEventListener('click', (evento)=>{
+    alert('Mi email profesional es "Luis_campoy18...."')
+})
+
+const botonTelef = document.getElementById('botontelefono')
+
+botonTelef.addEventListener('click', (evento)=>{
+    alert('Mi contacto de Whasapp y telefono es 666XXXXXX')
+})
+
